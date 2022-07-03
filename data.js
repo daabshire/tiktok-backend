2328dc7238e5c0483c128b789b1533bf20481b85
Dawn <67352124+daabshire@users.noreply.github.com>
2022-07-03 13:39:49 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url:"https://player.vimeo.com/external/433877873.sd.mp4?s=7c2f08c9a72c5f7cdab584080d221c921a08540e&profile_id=165&oauth2_token_id=57447761",
+        channel:"sssangha",
+        description:"YOOO this works",
+        song:"99 problems but REACT aint one!",
+        likes: "37921",
+        messages:"4654",
+        shares:"500",
+    },  
+    { 
+        url:"https://player.vimeo.com/external/513373860.hd.mp4?s=3aa9a256b9e4eea8b6ed071af5cf6052c3d88c69&profile_id=174&oauth2_token_id=57447761",
+        channel:"sssangha",
+        description:"YOOO this works",
+        song:"99 problems but REACT aint one!",
+        likes:"999",
+        messages:"854",
+        shares: "658",
+    },
+];
+
+export default data;
